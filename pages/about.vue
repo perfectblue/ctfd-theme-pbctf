369ca87c6643cb10624e04f0b2e6f0b161b41221
Sampriti Panda <sampritipanda@outlook.com>
2024-10-23 06:53:05 +0000

    prize

--- pages/about.vue
+++ pages/about.vue
@@ -8,17 +8,14 @@
 			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20241014T0200" target="_blank" rel="noopener">10/14 02:00</a> <small>UTC</small>
 		</p>
 
-		<!--
 		<h3>Prizes</h3>
 		<p> We have a total prize pool of 7,500 USD! 🏆 </p><br>
 		<ul>
-			<li><b>1st place</b> - 3,000 USD (2,000 USD cash + 500 USD x 2 Amazon vouchers), 5 Binary Ninja Personal License</li><br>
-			<li><b>2nd place</b> - 2,000 USD (1,500 USD cash + 500 USD Amazon voucher),  3 Binary Ninja Personal License</li><br>
-			<li><b>3rd place</b> - 1,000 USD (500 USD cash + 500 USD Amazon voucher), 1 Binary Ninja Personal License</li><br>
-     		<li><b>Writeups </b> - 1,400 USD (9 selected writeups x 100 USD cash for each, 1 selected writeup x 500 USD Amazon voucher)</li><br>
-			<li><b>BN Cloud Writeup </b> - 1 Binary Ninja personal license to best writeup showing Binary Ninja Cloud usage, 100 USD</li><br>
+		    <li><b>1st place</b> - 4,000 USD Cash</li><br>
+		    <li><b>2nd place</b> - 2,000 USD cash</li><br>
+		    <li><b>3rd place</b> - 1,000 USD cash </li><br>
+		    <li><b>Writeups </b> - 500 USD (5 selected writeups x 100 USD cash)</li><br>
 		</ul>
-		-->
 
 		<h3>Social</h3>
 		<ul class="socials">
